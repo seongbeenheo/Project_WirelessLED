18dfb29ba474d86b78318a7481d434982e64a455
SEONGBEEN HEO <102204711+seongbeenheo@users.noreply.github.com>
2023-08-15 15:39:26 +0900

    Update Project_Wireless_LED.ino
    1. IO connection attempt to set up connection abandon function after 30 seconds
    2. Change the NeoPixel RGB value so that each value can be set
    3. Multiple other optimization settings
    
    1. IO 연결시도 30초 경과시 연결포기 함수 설정
    2. NeoPixel RGB값 각각 설정 가능하게끔 변경
    3. 기타 여러 최적화 설정

--- Project_Wireless_LED/Project_Wireless_LED.ino
+++ Project_Wireless_LED/Project_Wireless_LED.ino
@@ -33,36 +33,76 @@ AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WiFi_ID, WiFi_PW);
 //Adafruit IO Feeds Import (to get data from Adafruit.IO)
 AdafruitIO_Feed *feed_color = io.feed("feed-color");
 AdafruitIO_Feed *feed_bright = io.feed("feed-bright");
+AdafruitIO_Feed *feed_color_r = io.feed("feed-color-r");
+AdafruitIO_Feed *feed_color_g = io.feed("feed-color-g");
+AdafruitIO_Feed *feed_color_b = io.feed("feed-color-b");
 
 AdafruitIO_Feed *feed_color_out = io.feed("feed-color-out");
 AdafruitIO_Feed *feed_bright_out = io.feed("feed-bright-out");
+AdafruitIO_Feed *feed_color_RGB_out = io.feed("feed-color-rgb-out");
 
 ESP8266WebServer server(80);  // WebServer Object : Set Server Port
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIXELPIN, NEO_GRB + NEO_KHZ800);  //NeoPixel Object, Please check the Neopixel Model ex> NEO_GRB
 
-std::map<String, uint32_t> ColorMap = {  //ColorMap Dataframe 
-  {"OFF", pixels.Color(0, 0, 0)},
-  {"RED", pixels.Color(255, 0, 0)},
-  {"GREEN", pixels.Color(0, 255, 0)},
-  {"BLUE", pixels.Color(0, 0, 255)},
-  {"ORANGE", pixels.Color(255, 165, 0)},
-  {"WHITE", pixels.Color(255, 255, 255)}
-};
-
 const char* ColorName[] = {"OFF", "RED", "GREEN", "BLUE", "ORANGE", "WHITE"}; //ColorName list
 const int numColorName = sizeof(ColorName) / sizeof(ColorName[0]); //ColorName list length 
+const int ColorValues[][3] = {  //saves the RGB values
+    {0, 0, 0},
+    {255, 0, 0},
+    {0, 255, 0},
+    {0, 0, 255},
+    {255, 165, 0},
+    {255, 255, 255}
+};
+
+std::map<String, uint32_t> ColorMap;  ////ColorMap Dataframe 
+
+std::map<String, String> RGBShort = {
+  {"R","RED"},
+  {"B","BLUE"},
+  {"G","GREEN"}
+};
+
+int findColorIndex(const char* color) {
+    for (int i = 0; i < numColorName; i++) {
+        if (strcmp(color, ColorName[i]) == 0) {
+            return i;
+        }
+    }
+    return -1; // if color not exists, return -1
+}
 
 //set Current ColorMode and Brightmode for global variables
 String ColorMode = "OFF";
-int BrightMode = 255;
+int BrightMode = 0;
+int Color_R = 0;
+int Color_G = 0;
+int Color_B = 0;
+String currentColorType = "";
+String Color_Total;
+int Color_Each = 0; 
 
-void WiFiSetup(){  //Connects WiFi
+void setColorMap() {
+  for (int i=0; i < numColorName; i++) {
+    uint32_t eachColorMap = pixels.Color(ColorValues[i][0], ColorValues[i][1], ColorValues[i][2]);
+    ColorMap.insert(std::make_pair(String(ColorName[i]), eachColorMap));
+  }
+}
+
+void setColortoRGB(String colortype) {
+  int colorindex = findColorIndex(colortype.c_str());
+  Color_R = ColorValues[colorindex][0];
+  Color_G = ColorValues[colorindex][1];
+  Color_B = ColorValues[colorindex][2];
+}
+
+void WiFiSetup() {  //Connects WiFi
   WiFi.begin(WiFi_ID, WiFi_PW);
 
   Serial.print(F("Connecting"));
   while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
+    delay(250);
     Serial.print(".");
   }
 
@@ -73,19 +113,28 @@ void WiFiSetup(){  //Connects WiFi
 
 void IOSetup() {  //Connects to Adafruit IO
   Serial.print(F("Connecting to Adafruit IO"));
+  unsigned long startTime = millis();
   io.connect();
   while (io.status() != AIO_CONNECTED) {
     Serial.print(F("."));
-    delay(500);
+    delay(250);
+    if (millis() - startTime > 30000) {
+      Serial.println();
+      Serial.println(F("Failed to connect to Adafruit IO. Timed out"));
+      break;
+    }
   }
-
   feed_color->onMessage(handleColorAdafruit);  //callback function
   feed_bright->onMessage(handleBrightAdafruit);
-
+  feed_color_r->onMessage(handleColor_Each_Adafruit);
+  feed_color_g->onMessage(handleColor_Each_Adafruit);
+  feed_color_b->onMessage(handleColor_Each_Adafruit);
   Serial.println();
   Serial.print(F("IO Status: "));
   Serial.println(io.statusText());
 }
+
+void handleColor_Each_http();
 
 void HTTPSetup() {  //Sets HTTP Server
   server.on("/", handleROOT); 
@@ -94,14 +143,21 @@ void HTTPSetup() {  //Sets HTTP Server
     String urllink = "/color/" + String(ColorName[i]);
     server.on(urllink, handleColorhttp);
   }
-  for (int i = 0; i < 256; i++) {
+  for (int i = 0; i < 256; i += 10) {
+    String urllink_R = "/color/R/" + String(i);
+    String urllink_G = "/color/G/" + String(i);
+    String urllink_B = "/color/B/" + String(i);
     String urllink = "/brightness/" + String(i);
+    server.on(urllink_R, handleColor_Each_http);
+    server.on(urllink_G, handleColor_Each_http);
+    server.on(urllink_B, handleColor_Each_http);
     server.on(urllink, handleBrighthttp);
   }
   server.on("/reboot",handleReboot);
+  server.on("/IOconnect",handleIOconnect);
   server.onNotFound(handleonNotFound);
   server.begin();
-  Serial.println(F("HTTP Server successfully operated"));
+  Serial.println(F("HTTP Server successfully operated."));
 }
 
 void PixelSetup() {  //Sets NeoPixel
@@ -113,8 +169,10 @@ void PixelSetup() {  //Sets NeoPixel
 }
 
 void CurrentSerial() {  //Shows Current ColorMode and Brightness to Serial
+  Color_Total = "   ( " + String(Color_R) + "," + String(Color_G) + "," + String(Color_B) + " )"; 
   Serial.print(F("Current ColorMode: "));
-  Serial.println(ColorMode);
+  Serial.print(ColorMode);
+  Serial.println(Color_Total);  
   Serial.print(F("Current Brightness: "));
   Serial.println(String(BrightMode));
 }
@@ -124,6 +182,7 @@ void setup() {  //The function that runs when the program starts
   Serial.begin(115200);
   delay(5000);
   Serial.println();
+  setColorMap();   //set Color values to ColorMap
   WiFiSetup();  //Connects WiFi
   IOSetup();  //Connects to Adafruit IO
   HTTPSetup();  //Sets HTTP Server
@@ -135,9 +194,9 @@ void setup() {  //The function that runs when the program starts
 void loop()  //The funciton that runs repeatedly
 {
   io.run();
-  if (io.mqttStatus() < AIO_CONNECTED) { //Auto Reconnect Adafruit IO when disconnected
+  if (io.status() != AIO_CONNECTED) { //Auto Reconnect Adafruit IO when disconnected
     Serial.print("Adafruit IO Disconnected. ");
-    IOSetup(); 
+    IOSetup();
   }
   server.handleClient(); // Handle HTTP Requests ( It helps to turn on the server)
 }
@@ -145,26 +204,46 @@ void loop()  //The funciton that runs repeatedly
 void IOsend() {
   feed_color_out->save(ColorMode);
   feed_bright_out->save(String(BrightMode));
+
+  Color_Total = "( " + String(Color_R) + "," + String(Color_G) + "," + String(Color_B) + " )";
+  feed_color_RGB_out->save(Color_Total);
 }
 
 // NeoPixel LED Settings ( Btightness and Color )
-void setColor(String pixelcolor) {  //Sets 
+void setColor(String pixel) {
+  ColorMode = pixel;
   for (int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, ColorMap[pixelcolor]);
+    pixels.setPixelColor(i, ColorMap[ColorMode]);
   }
   pixels.show();
-  ColorMode = pixelcolor;
+  setColortoRGB(ColorMode);
   IOsend();
   Serial.println("Color changed.");
   CurrentSerial();
 }
 
-void setBright(int pixelbright) {
-  pixels.setBrightness(pixelbright);
+void setColor_Each(String type, int eachpixel) {
+  if (type == "RED") {
+    Color_R = eachpixel;
+  } else if (type == "GREEN") {
+    Color_G = eachpixel;
+  } else if (type == "BLUE") {
+    Color_B = eachpixel;
+  }
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, Color_R, Color_G, Color_B);
+  }
   pixels.show();
-  BrightMode = pixelbright;
-  feed_color_out->save(ColorMode);
-  feed_bright_out->save(String(BrightMode));
+  ColorMode = "Custom";
+  IOsend();
+  Serial.println("Color changed.");
+  CurrentSerial();
+}
+
+void setBright(int pixel) {
+  BrightMode = pixel;
+  pixels.setBrightness(BrightMode);
+  pixels.show();
   IOsend();
   Serial.println("Brightness changed.");
   CurrentSerial();
@@ -181,9 +260,24 @@ void handleROOT() {  //Main Page on HTTP WebServer. Controls Brightness, Color, 
   htmlRoot += "  xhr.open('GET','/brightness/' + value, true);";
   htmlRoot += "  xhr.send();";
   htmlRoot += "}";
-  htmlRoot += "function setColorhtml(color) {";     //seCcolorhtml() javascript function
+  htmlRoot += "function setColorhtml(color) {";     //setColorhtml() javascript function
   htmlRoot += "  var xhr = new XMLHttpRequest();";
   htmlRoot += "  xhr.open('GET','/color/' + color, true);";
+  htmlRoot += "  xhr.send();";
+  htmlRoot += "}";
+  htmlRoot += "function setColor_R_html(value) {";    //setColor_R_html() javascript function
+  htmlRoot += "  var xhr = new XMLHttpRequest();";
+  htmlRoot += "  xhr.open('GET','/color/R/' + value, true);";
+  htmlRoot += "  xhr.send();";
+  htmlRoot += "}";
+  htmlRoot += "function setColor_G_html(value) {";    //setColor_G_html() javascript function
+  htmlRoot += "  var xhr = new XMLHttpRequest();";
+  htmlRoot += "  xhr.open('GET','/color/G/' + value, true);";
+  htmlRoot += "  xhr.send();";
+  htmlRoot += "}";
+  htmlRoot += "function setColor_B_html(value) {";    //setColor_B_html() javascript function
+  htmlRoot += "  var xhr = new XMLHttpRequest();";
+  htmlRoot += "  xhr.open('GET','/color/B/' + value, true);";
   htmlRoot += "  xhr.send();";
   htmlRoot += "}";
   htmlRoot += "</script>";
@@ -193,19 +287,37 @@ void handleROOT() {  //Main Page on HTTP WebServer. Controls Brightness, Color, 
   htmlRoot += "<h2></h2>";
   htmlRoot += "<h2>Current Color Mode : " + ColorMode + "</h2>";
   htmlRoot += "<h2>Current Brightness : " + String(BrightMode) + "</h2>";
+  htmlRoot += "<h2>Current RGB : ( " + String(Color_R) + ", " + String(Color_G) + ", " + String(Color_B) + " )</h2>";
   htmlRoot += "<h2>Set LED Color</h2>";
   htmlRoot += "<ul>";
   for (int i = 0; i < numColorName; i++) {
-    htmlRoot += "<li><a href=\"javascript:setColorhtml('" + String(ColorName[i]) + "')\">" + ColorName[i] + "</a></li>";
+      htmlRoot += "<li><a href=\"javascript:setColorhtml('" + String(ColorName[i]) + "')\">" + ColorName[i] + "</a></li>";
   }
   htmlRoot += "</ul>";
   htmlRoot += "<h2>Set LED Brightness</h2>";
   htmlRoot += "<ul>";
-  htmlRoot += "<input type='range' min='0' max='255' value='" + String(BrightMode) + "' onchange='setBrighthtml(this.value)'>";
+  htmlRoot += "<input type='range' min='0' max='255' value='" + String(BrightMode) + "' onchange='setBrighthtml(this.value)' step='10'>";
   htmlRoot += "<span>" + String(BrightMode) + "</span>";
+  htmlRoot += "</ul>";
+  htmlRoot += "<h2>Set LED RGB Color</h2>";
+  htmlRoot += "<h3>Red</h3>";
+  htmlRoot += "<ul>";
+  htmlRoot += "<input type='range' min='0' max='255' value='" + String(Color_R) + "' onchange='setColor_R_html(this.value)' step='10'>";
+  htmlRoot += "<span>" + String(Color_R) + "</span>";
+  htmlRoot += "</ul>";
+  htmlRoot += "<h3>Green</h3>";
+  htmlRoot += "<ul>";
+  htmlRoot += "<input type='range' min='0' max='255' value='" + String(Color_G) + "' onchange='setColor_G_html(this.value)' step='10'>";
+  htmlRoot += "<span>" + String(Color_G) + "</span>";
+  htmlRoot += "</ul>";
+  htmlRoot += "<h3>Blue</h3>";
+  htmlRoot += "<ul>";
+  htmlRoot += "<input type='range' min='0' max='255' value='" + String(Color_B) + "' onchange='setColor_B_html(this.value)' step='10'>";
+  htmlRoot += "<span>" + String(Color_B) + "</span>";
   htmlRoot += "</ul>";
   htmlRoot += "<h2></h2>";
   htmlRoot += "<h2><a href=\"/reboot\">REBOOT!!</a></h2>";
+  htmlRoot += "<h2><a href=\"/IOconnect\">Adafruit IO Reconnect!!</a></h2>";
   htmlRoot += "</body>";
   htmlRoot += "</html>";
   server.send(200, "text/html", htmlRoot);
@@ -222,15 +334,28 @@ void handleonNotFound() {  //Shows when user tries to access a page that does no
 }
 void handleReboot() {  //Reboots the ESP8266 for developers on HTTP WebServer
   String htmlReboot = "<html>";
-  htmlReboot += "<head><meta http-equiv='refresh' content='3;url=/'><title>Rebooting...</title>";
+  htmlReboot += "<head><title>Rebooting...</title>";
   htmlReboot += "</head><body>";
   htmlReboot += "<h1>Rebooting...</h1>";
-  htmlReboot += "<p>Redirecting to main site in 3 seconds...</p>";
   htmlReboot += "</body>";
   htmlReboot += "</html>";
   server.send(200, "text/html", htmlReboot);
+  delay(3000);
   Serial.println(F("Rebooting..."));
   ESP.restart(); //reboot ESP8266
+}
+
+void handleIOconnect() {  //Reconnect to Adafruit IO
+  String htmlIOconnect = "<html>";
+  htmlIOconnect += "<head><meta http-equiv='refresh' content='15;url=/'><title>Reconnecting to Adafruit IO...</title>";
+  htmlIOconnect += "</head><body>";
+  htmlIOconnect += "<h1>Reconnecting to Adafruit IO...</h1>";
+  htmlIOconnect += "</body>";
+  htmlIOconnect += "</html>";
+  server.send(200, "text/html", htmlIOconnect);
+  delay(3000);
+  Serial.print(F("By HTTP, "));
+  IOSetup();
 }
 
 //html for handleColorhttp and handleBrighthttp on HTTP WebServer
@@ -244,6 +369,18 @@ void htmlSetting(String option, String optionmode) {  //only String ex> color, R
   htmlset += "</body>";
   htmlset += "</html>";
   server.send(200, "text/html", htmlset);
+}
+
+void htmlSetColorCustom(String type, String optionmode) {  //only String ex> RED, 255 / BLUE, 132
+  String htmlSetCustom = "<html>";
+  htmlSetCustom += "<head><meta http-equiv='refresh' content='3;url=/'><title>ESP8266 Custom Color ("+ type + ") Set : " + optionmode + "</title>";
+  htmlSetCustom += "</head><body>";
+  htmlSetCustom += "<h1> Custom Color (" + type + ") Set to :" + optionmode + "!</h1>";
+  htmlSetCustom += "<h2></h2>";
+  htmlSetCustom += "<li><a href=\"/\">Go Back to Main Site</a></li>";
+  htmlSetCustom += "</body>";
+  htmlSetCustom += "</html>";
+  server.send(200, "text/html", htmlSetCustom);
 }
 
 void htmlInvaild(String option) {  //only String ex> color / brightness
@@ -284,14 +421,32 @@ void handleColorhttp() {  //Controls the color of NeoPixel LED on HTTP WebServer
     Serial.println();
   } else {
     htmlInvaild(setting);
+  };
+}
+
+void handleColor_Each_http() {  //Controls the custom color(Red) of NeoPixel LED on HTTP WebServer
+  String uri = server.uri();
+  String type_S = uri.substring(7,8);
+  String type = String(RGBShort[type_S]);
+  String color_string = uri.substring(9); //String
+  int color = color_string.toInt();  //int
+  color = color - (color % 10);
+  if (color >= 0 && color <= 255) {
+    setColor_Each(type,color);
+    htmlSetColorCustom(type, String(color));
+    Serial.println(F("Operated by HTTP"));
+    Serial.println();
+  } else {
+    htmlInvaild("custom color");
   }
 }
 
 void handleBrighthttp() {  //Controls the Brightness of NeoPixel LED on HTTP WebServer
   String url = server.uri();
-  String bright_string = url.substring(12); //string
+  String bright_string = url.substring(12); //String
   int bright = bright_string.toInt();  //int
-  String setting = "brightness";
+  bright = bright - (bright % 10);
+   String setting = "brightness";
   if (bright >= 0 && bright <= 255) {
     setBright(bright);
     htmlSetting(setting, String(BrightMode));
@@ -313,9 +468,30 @@ void handleColorAdafruit(AdafruitIO_Data *data) {  //Controls the Color of NeoPi
   }
 }
 
+void handleColor_Each_Adafruit(AdafruitIO_Data *data) {  //Controls the Custom Color of NeoPixel LED on Adafruit.IO
+  String feedKey = data->feedName();
+  String colortype = feedKey.substring(11);
+  String Color_String = data->value();
+  Color_Each = Color_String.toInt();
+  Color_Each = Color_Each - (Color_Each % 10);
+  if (colortype == "r"){
+    colortype = "RED";
+  } else if (colortype == "g"){
+    colortype = "GREEN";
+  } else if (colortype == "b"){
+    colortype = "BLUE";
+  }
+  if (Color_Each >= 0 && Color_Each <= 255 && (colortype == "RED" || colortype == "GREEN" || colortype == "BLUE")) {
+    setColor_Each(colortype,Color_Each);
+    Serial.println(F("Operated by Adafruit.io"));
+    Serial.println();
+  }
+}
+
 void handleBrightAdafruit(AdafruitIO_Data *data) {  //Controls the Brightness of NeoPixel LED on Adafruit.IO
   String Bright_Adafruit_String = data->value();
   BrightMode = Bright_Adafruit_String.toInt();
+  BrightMode = BrightMode - (BrightMode % 10);
   if (BrightMode >= 0 && BrightMode <= 255) {
     setBright(BrightMode);
     Serial.println(F("Operated by Adafruit.io"));
